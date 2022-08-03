2101bb675f997fec311e2ec8b064e2c758d80389
rodalexanderson <alexanderson.ibarra@gmail.com>
2022-08-02 20:23:16 -0500

    new images (tunnels and characters) and easter egg  added, blog deleted from navbar, css adjusted in my skills section

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// MOUNTAIN SECTION ANIMATION STARTS
+
 const nube = document.getElementById('nube')
 const nube1 = document.getElementById('nube1')
 const nube2 = document.getElementById('nube2')
@@ -13,7 +15,6 @@ nube2.style.bottom = nube2YBottom + '%'
 nube3.style.bottom = nube1YBottom + '%'
 arbusto1.style.left = arbusto1YLeft + '%'
 
-
 function move() {
     const incrementer = window.scrollY
     nube.style.bottom = nubeYBottom + incrementer * 0.1 + '%'
@@ -24,13 +25,10 @@ function move() {
 }
 
 window.addEventListener('scroll', move)
+// MOUNTAIN SECTION ANIMATION ENDS
 
 
-const cloudthought = document.getElementById('cloud-thought')
-const cloudthought1 = document.getElementById('cloud-thought1')
-const cloudthought2 = document.getElementById('cloud-thought2')
-const cloudthought3 = document.getElementById('cloud-thought3')
-
+// EASTER EGGS MARIO STARTS
 function showMario() {
     const mario2 = document.getElementById('mario')
     if (mario2.style.display = "none"){
@@ -41,12 +39,35 @@ function showMario() {
 }
 
 function showMario2() {
-    const head = document.getElementById('head')
+    const control = document.getElementById('control')
     const mario2 = document.getElementById('mario2')
     if (mario2.style.display = "none"){
-        (mario2.style.display = "block") && (head.style.display = "none")
+        (mario2.style.display = "block") && (control.style.display = "none")
     } else {
         mario2.style.display = "none"
     }
 }
 
+// EASTER EGGS MARIO END
+
+// MY SKILLS ANIMATION STARTS
+function ghostShown() {
+    const ghost = document.getElementById('ghost')
+    ghost.style.top = -20 + '%' 
+}
+function monedaShown() {
+    const moneda = document.getElementById('moneda')
+    moneda.style.top = -20 + '%' 
+}
+function flowerShown() {
+    const flower = document.getElementById('flower')
+    flower.style.top = -20 + '%' 
+}
+function starShown() {
+    const star = document.getElementById('star')
+    star.style.top = -20 + '%' 
+}
+function hongoShown() {
+    const hongo = document.getElementById('hongo')
+    hongo.style.top = -20 + '%' 
+}
