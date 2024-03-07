01ec559ba6d2219a64439b66e039819af04942e0
rodalexanderson <73920054+rodalexanderson@users.noreply.github.com>
2024-03-07 01:08:49 -0600

    changes in projects, and overall text

--- portfolio.js
+++ portfolio.js
@@ -21,6 +21,7 @@ function move() {
     nube1.style.bottom = nubeYBottom + incrementer * 0.2 + '%'
     nube2.style.bottom = nubeYBottom + incrementer * 0.1 + '%'
     nube3.style.bottom = nubeYBottom + incrementer * 0.2 + '%'
+    nube1.style.left = nubeYBottom + incrementer * 0.2 + '%'
     arbusto1.style.left = arbusto1YLeft + incrementer * 0.005 + '%'
 }
 
